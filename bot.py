b99f0bd9fa4b41c615510d9d21c3df37f24b3184
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2026-03-13 08:48:41 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ API_HASH = "3037d39233c6fad9b80d83bb8a339a07"
 BOT_TOKEN = "7350676839:AAGlgApZke3RNYzS2ggLBdJjOiBmOq7Tq_U"
 SESSION_STRING = "BQGZqnkAWSiUjFwWgWKFAeiczK7pIa_hunvbXRbfI6sl_71r0AwPsmvi9QFOo2ziKqG8_-OouWLeWephqt58W8Qwi4rQ0BhlSUTQmn8kl6V2a6_w4Su-SjRwpF_tZH89Kf2nEccI7CmDl6bxRDF8ce0reeRsdMU0p1HTzMQLlb87WWRsVT9WQjW9cM9-NwHlm5MvEasYdH2wsPqaI3H92AqdfO5E1HjX2jIrF1G-okyGgGEDAropP3ir10bFXWn5uGFOaUcO1TSJSywMHQF32KXyJj1yaIT3hwcjdwE7OQl62mBRNhye0DT7AI55DAC2IQr_ypMzV0FOODEMCEY_VUJuEW6amgAAAAHRJC-ZAA"
 
-A_CHAT = -1001234567890   # Chat ID jaha links detect karne hain
+A_CHAT = -1002513087490   # Chat ID jaha links detect karne hain
 FILES_DIR = "./files"     # Directory jaha files stored hain
 
 # ==========================================
